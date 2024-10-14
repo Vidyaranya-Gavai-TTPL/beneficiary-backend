db6cb84f3134823b27f6fecf13e92d1326aadcd1
ManojNathIC <manoj_n@tekditechnologies.com>
2024-10-14 11:30:22 +0530

    Task #0000: Removed version and user docs API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -13,13 +13,13 @@ import { User } from '../../entity/user.entity';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDocDTO } from './dto/user_docs.dto';
 
 @ApiTags('users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Version('1')
   @Post('/create')
   @ApiOperation({ summary: 'Create a new user' })
   @ApiResponse({ status: 201, description: 'User successfully created' })
@@ -28,7 +28,6 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Version('1')
   @Put('/update/:userId')
   @ApiOperation({ summary: 'Update an existing user' })
   @ApiResponse({ status: 200, description: 'User successfully updated' })
@@ -40,7 +39,6 @@ export class UserController {
     return this.userService.update(userId, updateUserDto);
   }
 
-  @Version('1')
   @Get('/get_one/:userId')
   @ApiOperation({ summary: 'Get a user by userId' })
   @ApiResponse({ status: 200, description: 'User data' })
@@ -49,12 +47,19 @@ export class UserController {
     return this.userService.findOne(userId);
   }
 
-  @Version('1')
   @Delete('/delete/:userId')
   @ApiOperation({ summary: 'Delete a user by userId' })
   @ApiResponse({ status: 200, description: 'User successfully deleted' })
   @ApiResponse({ status: 404, description: 'User not found' })
   async remove(@Param('userId') userId: string): Promise<void> {
     return this.userService.remove(userId);
+  }
+
+  @Post('/user_docs')
+  @ApiOperation({ summary: 'Save user docs' })
+  @ApiResponse({ status: 200, description: 'User docs saved successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  async createUserDoc(@Body() createUserDocDto: CreateUserDocDTO) {
+    return this.userService.createUserDoc(createUserDocDto);
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from '@modules/users/users.controller';
 import { UserService } from '@modules/users/users.service';
 import { User } from '@entities/user.entity';
+import { UserDoc } from '@entities/user_docs.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserDoc])],
   controllers: [UserController],
   providers: [UserService],
 })
