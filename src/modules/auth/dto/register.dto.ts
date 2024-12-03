05ca061e95d48558f17e3c4318c6c46a10288e66
ManojNathIC <manoj_n@tekditechnologies.com>
2024-12-03 16:14:15 +0530

    BE-commented pwd validation and handled error message key

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -30,15 +30,15 @@ export class RegisterDTO {
 
   @ApiProperty({})
   @IsOptional()
-  @ValidateIf((obj) => obj.password !== undefined && obj.password !== null)
-  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-    {
-      message:
-        'Password must contain uppercase, lowercase, number, and special character',
-    },
-  )
+  // @ValidateIf((obj) => obj.password !== undefined && obj.password !== null)
+  // @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
+  // @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  // @Matches(
+  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+  //   {
+  //     message:
+  //       'Password must contain uppercase, lowercase, number, and special character',
+  //   },
+  // )
   password: string;
 }
