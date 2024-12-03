96b1992739af5268e05abd7ef6fadd67d3e980ea
ManojNathIC <manoj_n@tekditechnologies.com>
2024-12-03 16:22:40 +0530

    BE-commented pwd validation

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -32,7 +32,7 @@ export class RegisterDTO {
   @IsOptional()
   // @ValidateIf((obj) => obj.password !== undefined && obj.password !== null)
   // @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
-  // @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MinLength(4, { message: 'Password must be at least 4 characters long' })
   // @Matches(
   //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
   //   {
