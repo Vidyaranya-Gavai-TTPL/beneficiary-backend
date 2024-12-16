8ca1bfa0cd634f423fb66b606ab328d4f19aa78a
Vidyaranya-Gavai-TTPL <vidyaranya.gavai@tekditechnologies.com>
2024-12-16 14:43:51 +0530

    Overwrite Existing Document

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -490,6 +490,8 @@ export class UserService {
       const { userProfile, validationData } =
         await this.profilePopulator.buildProfile(VCs);
 
+      console.log(userProfile, validationData);
+
       // Update database entries
       await this.profilePopulator.updateDatabase(
         userProfile,
@@ -513,7 +515,6 @@ export class UserService {
     const baseFolder = path.join(__dirname, 'userData'); // Base folder for storing user files
 
     const savedDocs: UserDoc[] = [];
-    const existingDocs: UserDoc[] = [];
 
     // Ensure the `userData` folder exists
     if (!fs.existsSync(baseFolder)) {
@@ -536,39 +537,49 @@ export class UserService {
       });
 
       if (existingDoc) {
-        existingDocs.push(existingDoc);
-        console.log(
-          `Document already exists for user_id: ${createUserDocDto.user_id}, doc_type: ${createUserDocDto.doc_type}, doc_subtype: ${createUserDocDto.doc_subtype}`,
-        );
-      } else {
-        if (
-          createUserDocDto.doc_data &&
-          typeof createUserDocDto.doc_data !== 'string'
-        ) {
-          const jsonDataString = JSON.stringify(createUserDocDto.doc_data);
-
-          // Encrypt the JSON string
-          createUserDocDto.doc_data =
-            this.encryptionService.encrypt(jsonDataString);
+        const queryRunner =
+          this.userDocsRepository.manager.connection.createQueryRunner();
+        await queryRunner.connect();
+        try {
+          await queryRunner.startTransaction();
+          await queryRunner.manager.remove(existingDoc);
+          await queryRunner.commitTransaction();
+        } catch (error) {
+          Logger.error('Error while deleting the document: ', error);
+          await queryRunner.release();
+          throw new ErrorResponse({
+            statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+            errorMessage: `Error while deleting the document: ${error}`,
+          });
+        } finally {
+          await queryRunner.release();
         }
-        if (!createUserDocDto?.user_id) {
-          createUserDocDto.user_id = userDetails?.user_id;
-        }
-
-        // Create the new document entity for the database
-        const savedDoc = await this.saveDoc(createUserDocDto);
-        savedDocs.push(savedDoc);
-
-        await this.writeToFile(createUserDocDto, userFilePath, savedDoc);
       }
+
+      if (
+        createUserDocDto.doc_data &&
+        typeof createUserDocDto.doc_data !== 'string'
+      ) {
+        const jsonDataString = JSON.stringify(createUserDocDto.doc_data);
+
+        // Encrypt the JSON string
+        createUserDocDto.doc_data =
+          this.encryptionService.encrypt(jsonDataString);
+      }
+      if (!createUserDocDto?.user_id) {
+        createUserDocDto.user_id = userDetails?.user_id;
+      }
+
+      // Create the new document entity for the database
+      const savedDoc = await this.saveDoc(createUserDocDto);
+      savedDocs.push(savedDoc);
+
+      await this.writeToFile(createUserDocDto, userFilePath, savedDoc);
     }
 
     // Update profile based on documents
     await this.updateProfile(userDetails);
 
-    if (existingDocs.length > 0) {
-      return existingDocs;
-    }
     return savedDocs;
   }
   // User info
